d496b46b0302cc5665fbd9acab3b0f7214e737a6
hitoTright <915840223@qq.com>
2017-07-26 22:23:36 +0800

    添加项目文件。

---
+++ Nodepad/Nodepad.h
@@ -0,0 +1,32 @@
+
+// Nodepad.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CNodepadApp: 
+// 有关此类的实现，请参阅 Nodepad.cpp
+//
+
+class CNodepadApp : public CWinApp
+{
+public:
+	CNodepadApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CNodepadApp theApp;
